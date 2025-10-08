6664bd76c9b094b1156607966b5da136ecf72f2c
Paolo Pironi <ppiron@protonmail.com>
2025-10-08 12:27:25 +0000

    Create base skeleton

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+require('includes/nav.php');
+?>
+<!-- Hero Image -->
+<div class="position-relative">
+    <img src="assets/hero_image.png" alt="" class="img-fluid w-100 d-none d-md-inline">
+    <img src="assets/hero_image_black.png" alt="" class="img-fluid w-100 d-md-none">
+    <div class="hero-text text-light">
+        <h2 class="fw-bolder fs-1">Mk Time</h2>
+        <p class="fw-medium fs-4">Timepieces of Excellence</p>
+        <div class="">
+            <a href="products.html" class="hero-button btn btn-outline-light text-uppercase mt-2">Shop
+                now</a>
+        </div>
+    </div>
+
+</div>
+<?php
+require('includes/footer.php');
+?>
