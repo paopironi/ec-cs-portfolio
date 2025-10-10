d06a01fa185b4529b0fd94571c90dad4f764e212
Paolo Pironi <ppiron@protonmail.com>
2025-10-10 14:29:45 +0000

    Create products page

---
+++ products.php
@@ -0,0 +1,75 @@
+<?php
+session_start();
+$title = 'Shop';
+$user = $_SESSION['first_name'] ?? null;
+if (!$user) {
+    header("location: https://" . $_SERVER["HTTP_HOST"] . "/login.php");
+    exit;
+}
+require('connect_db.php');
+$query = "select * from products limit 18";
+$r = mysqli_query($link, $query);
+$rows = mysqli_fetch_all($r, MYSQLI_ASSOC);
+$nprods = $r->num_rows;
+$table_rows = $nprods % 4 == 0 ? $nprods / 4 : floor($nprods / 4) + 1;
+require('includes/nav.php');
+?>
+<!-- Products -->
+<section class="container my-5">
+    <h3 class="text-uppercase text-center">Our collection</h3>
+    <?php
+    $n = 1;
+    foreach ($rows as $row) {
+        $item_in_cart = array_key_exists(
+            $row['item_id'],
+            $_SESSION['cart']
+
+        );
+        $n_stars = random_int(3, 5);
+        $n_reviews = random_int(35, 105);
+        if ($n % 4 == 1) {
+    ?>
+            <div class="row mt-3 g-3">
+            <?php
+        }
+            ?>
+            <div class="col-lg-3 col-md-6 d-flex flex-column justify-content-between">
+                <div class="mt-3 text-center border border-secondary-subtle border-opacity-25 py-4">
+                    <img src="assets/<?= $row['item_img']; ?>" alt="<?= $row['item_name']; ?>" class="img-fluid" style="height: 150px;">
+                </div>
+                <div class="mt-3 px-5 px-md-4">
+                    <p class="fst-italic"><?= $row['item_name']; ?></p>
+                    <p class="mt-3 fw-bold">£<?= $row['item_price']; ?></p>
+                    <p class="feat-product-text lh-sm text-secondary"><?= $row['item_desc']; ?></p>
+                    <div>
+                        <?php
+                        for ($i = 1; $i <= $n_stars; $i++) {
+                            echo '<i class="bi bi-star-fill"></i>';
+                        }
+                        for ($i = 5 - $n_stars; $i >= 1; $i--) {
+                            echo '<i class="bi bi-star"></i>';
+                        }
+                        ?>
+                        <span class="ms-2">(<?= $n_reviews; ?>)</span>
+                    </div>
+                    <div>
+                        <a href="added.php?id=<?= $row['item_id']; ?>" class="<?= $item_in_cart ? 'in-cart-button' : 'hero-button'; ?> btn btn-outline text-uppercase mt-4">Add to Cart</a>
+                        <!-- <form action="added.php?id=<?= $row['item_id']; ?>" method="GET">
+                            <button type="submit" class="<?= $item_in_cart ? 'in-cart-button' : 'hero-button'; ?> btn btn-outline text-uppercase mt-4"><?= $item_in_cart ? 'View Cart' : 'Add To Cart'; ?></button>
+                        </form> -->
+                    </div>
+                </div>
+            </div>
+            <?php
+            if ($n % 4 == 0) {
+            ?>
+            </div>
+    <?php
+            }
+            $n = $n + 1;
+        }
+    ?>
+</section>
+<?php
+require('includes/footer.php');
+?>
