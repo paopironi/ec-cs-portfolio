fed82995659c961c64235132a2879a547bc29ab7
Paolo Pironi <ppiron@protonmail.com>
2025-10-12 07:36:32 +0000

    Protect route and some cosmetic changes

--- cart.php
+++ cart.php
@@ -1,12 +1,12 @@
 <?php
 session_start();
 $user = $_SESSION['first_name'] ?? null;
+if (!$user) {
+    header("location: https://" . $_SERVER["HTTP_HOST"] . "/login.php");
+    exit;
+}
 $title = 'Cart';
-// echo '<pre>' . var_dump($_POST['item_id']) . '</pre>';
-
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['delete-button'])) {
-    // var_dump($_POST);
-    // die();
     $id = $_POST['item_id'];
     $_SESSION['cart'][$id]['quantity'] = 0;
     unset($_SESSION['cart'][$id]);
@@ -18,8 +18,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['delete-button'])) {
         }
     }
 }
-// var_dump($_SESSION['cart']);
-// die();
 require('includes/nav.php');
 $nprods = count($_SESSION['cart']);
 ?>
@@ -41,53 +39,55 @@ $nprods = count($_SESSION['cart']);
         <h3 class="text-uppercase">Your Cart</h3>
         <div class="">
             <div style="width: 768px;">
-                <table class="table table-borderless mt-5">
-                    <thead>
-                        <tr>
-                            <th scope="col" style="width: 40%; text-align: left;">Product</th>
-                            <th scope="col" style="width: 10%; text-align: center;">Quantity</th>
-                            <th scope="col" style="width: 35%; text-align: right;">Subtotal</th>
-                            <th scope="col" style="width: 15%; text-align: right;"></th>
-                        </tr>
-                    </thead>
-                    <form action="" method="POST" id="cart-update">
-                        <tbody>
-                            <?php
-                            foreach ($rows as $row) {
-                                if ($_SESSION['cart'][$row['item_id']]['quantity'] > 0) {
-                            ?>
+                <div class="pb-4 border-bottom">
+                    <table class="table table-borderless mt-5">
+                        <thead>
+                            <tr>
+                                <th scope="col" style="width: 40%; text-align: left;">Product</th>
+                                <th scope="col" style="width: 10%; text-align: center;">Quantity</th>
+                                <th scope="col" style="width: 35%; text-align: right;">Subtotal</th>
+                                <th scope="col" style="width: 15%; text-align: right;"></th>
+                            </tr>
+                        </thead>
+                        <form action="" method="POST" id="cart-update">
+                            <tbody>
+                                <?php
+                                foreach ($rows as $row) {
+                                    if ($_SESSION['cart'][$row['item_id']]['quantity'] > 0) {
+                                ?>
 
-                                    <input type="hidden" name="item_id[]" value="<?= $row['item_id']; ?>" form="cart-update">
-                                    <tr class="">
-                                        <td style="width: 40%; text-align: left; vertical-align:middle"><?= $row['item_name']; ?></td>
+                                        <input type="hidden" name="item_id[]" value="<?= $row['item_id']; ?>" form="cart-update">
+                                        <tr class="">
+                                            <td style="width: 40%; text-align: left; vertical-align:middle"><?= $row['item_name']; ?></td>
 
-                                        <td style="width: 10%; text-align: center; vertical-align:middle"><input type="number" class="form-control" name="quantity[]" form="cart-update" min="0" value="<?= $_SESSION['cart'][$row['item_id']]['quantity']; ?>" style="text-align: center; width: 100px"></td>
+                                            <td style="width: 10%; text-align: center; vertical-align:middle"><input type="number" class="form-control" name="quantity[]" form="cart-update" min="0" value="<?= $_SESSION['cart'][$row['item_id']]['quantity']; ?>" style="text-align: center; width: 100px"></td>
 
-                                        <td style="width: 35%; text-align: right; vertical-align:middle">&pound;<?= number_format($_SESSION['cart'][$row['item_id']]['quantity'] * $row['item_price'], 2); ?></td>
+                                            <td style="width: 35%; text-align: right; vertical-align:middle">&pound;<?= number_format($_SESSION['cart'][$row['item_id']]['quantity'] * $row['item_price'], 2); ?></td>
 
-                                        <td style="width: 15%; text-align: right; font-size: small; vertical-align:middle">
+                                            <td style="width: 15%; text-align: right; font-size: small; vertical-align:middle">
 
-                                            <input type="hidden" name="item_id" form="delete-from-cart" value="<?= $row['item_id']; ?>">
-                                            <button type="submit" class="btn delete-button" form="delete-from-cart" name="delete-button"><i class="bi bi-trash"></i></button>
+                                                <input type="hidden" name="item_id" form="delete-from-cart" value="<?= $row['item_id']; ?>">
+                                                <button type="submit" class="btn delete-button" form="delete-from-cart" name="delete-button"><i class="bi bi-trash"></i></button>
 
-                                        </td>
+                                            </td>
 
-                                    </tr>
-                            <?php
+                                        </tr>
+                                <?php
+                                    }
                                 }
-                            }
-                            ?>
-                        </tbody>
-                    </form>
-                </table>
-                <div class="d-flex justify-content-end mt-5">
-                    <button class="btn in-cart-button" type="submit" form="cart-update">Update Cart</button>
+                                ?>
+                            </tbody>
+                        </form>
+                    </table>
+                    <div class="d-flex justify-content-end mt-5">
+                        <button class="btn in-cart-button" type="submit" form="cart-update">Update Cart</button>
+                    </div>
                 </div>
                 <?php
                 $cart = $_SESSION['cart'];
                 $cart_total = array_reduce($cart, fn($acc, $ele) => $acc + $ele['quantity'] * $ele['price'], 0)
                 ?>
-                <table class="table table-borderless mt-5">
+                <table class="table table-borderless mt-3">
                     <thead>
                         <tr>
                             <th scope="col" style="width: 40%; text-align: left;"></th>
@@ -106,7 +106,9 @@ $nprods = count($_SESSION['cart']);
                     </tbody>
                 </table>
                 <div class="d-flex justify-content-end mt-2">
-                    <button class="btn in-cart-button" type="submit" form="cart-submit">Place Order</button>
+                    <form action="order.php" method="POST" id="cart-submit">
+                        <button class="btn in-cart-button" type="submit" form="cart-submit">Place Order</button>
+                    </form>
                 </div>
             </div>
 
