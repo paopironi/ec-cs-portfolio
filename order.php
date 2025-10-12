93ea64d1e45656d2049bb1e45f62d05cb9336407
Paolo Pironi <ppiron@protonmail.com>
2025-10-12 07:37:51 +0000

    Implement order flow

---
+++ order.php
@@ -0,0 +1,81 @@
+<?php
+session_start();
+$title = 'Order';
+$user = $_SESSION['first_name'] ?? null;
+$user_id = $_SESSION['user_id'] ?? null;
+if (!$user) {
+    header("location: https://" . $_SERVER["HTTP_HOST"] . "/login.php");
+    exit;
+}
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && $user_id) {
+    $cart = $_SESSION['cart'];
+    $items_total = array_reduce($cart, fn($acc, $ele) => $acc + $ele['quantity'], 0);
+    $cart_total = array_reduce($cart, fn($acc, $ele) => $acc + $ele['quantity'] * $ele['price'], 0);
+    $now = date('Y-m-d H:i:s');
+
+    require("connect_db.php");
+
+    $query = mysqli_prepare($link, "insert into orders (user_id, total, order_date) values (?, ?, ?)");
+    mysqli_stmt_bind_param($query, "ids", $user_id, $cart_total, $now);
+    $created = @mysqli_stmt_execute($query);
+    if ($created) {
+        $created_order_id = mysqli_insert_id($link);
+        // echo $created_order_id;
+        // die();
+        foreach ($cart as $id => $item) {
+            $query = mysqli_prepare($link, "insert into order_contents (order_id, item_id, quantity, price) values (?, ?, ?, ?)");
+            mysqli_stmt_bind_param($query, "iiid", $created_order_id, $id, $item['quantity'], $item['price']);
+            $inserted = @mysqli_stmt_execute($query);
+            if ($inserted) {
+                unset($_SESSION['cart'][$id]);
+            } else {
+                $item_insertion_error[] = $id;
+            }
+        }
+    } else {
+        $ordering_error = true;
+    }
+    require("includes/nav.php");
+    mysqli_close($link);
+    if (!isset($ordering_error) && empty($item_insertion_error)) {
+?>
+        <div class="container py-3">
+            <div class="alert alert-success" role="alert">
+                <p>Success: Order #<?= $created_order_id ?> placed for <?= $items_total; ?> item(s). </p>
+                <p>Go to your <a href="orders.php">orders.</a></p>
+            </div>
+        </div>
+    <?php
+    } else if (isset($ordering_error)) {
+    ?>
+        <div class="container py-3">
+            <div class="alert alert-warning" role="alert">
+                <p>There was a problem creating an order for your items. Plese try again later.</p>
+            </div>
+        </div>
+    <?php
+    } else {
+    ?>
+        <div class="container py-3">
+            <div class="alert alert-warning" role="alert">
+                <p>There was a problem creating an order for the following item(s):</p>
+                <ul>
+                    <?php
+                    foreach ($item_insertion_error as $id) {
+                    ?>
+                        <li>
+                            <?= $id; ?>
+                        </li>
+                    <?php
+                    }
+                    ?>
+                </ul>
+                <p>Plese try again later.</p>
+            </div>
+        </div>
+<?php
+    }
+    require("includes/footer.php");
+} else {
+    die();
+}
